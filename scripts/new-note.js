c643ca8a07bd899b264dcec75d70e729fd69a86b
Roman <29733781+rzagreb@users.noreply.github.com>
2025-03-23 18:06:27 -0700

    Update new-note.js
    Changed template filename

--- scripts/new-note.js
+++ scripts/new-note.js
@@ -37,24 +37,40 @@ function run(argv) {
 	} catch (_error) {}
 
 	//───────────────────────────────────────────────────────────────────────────
+	const now = new Date();
+	const hours = now.getHours().toString().padStart(2, '0');
+	const minutes = now.getMinutes().toString().padStart(2, '0');
+	const seconds = now.getSeconds().toString().padStart(2, '0');
+	const cur_time = hours + minutes + seconds;
 
-	let fileName = argv[0]?.trim() || "Untitled";
-	fileName = fileName.replace(/[\\/:]/g, ""); // remove illegal characters
-	fileName = fileName.charAt(0).toUpperCase() + fileName.slice(1); // capitalize
+	let title = argv[0]?.trim() || cur_time;
+	let title_norm = title.toLowerCase().replace(/\s+/g, "_").replace(/[^\w-]/gi, "")
+
+	let fileName_base = title_norm;
+	fileName_base = fileName_base.replace(/[\\/:]/g, ""); // remove illegal characters
+	//fileName_base = fileName_base.charAt(0).toUpperCase() + fileName_base.slice(1); // capitalize
+
+	// Add current date prefix in YYMMDD_ format
+	const currentDate = new Date();
+	const year = String(currentDate.getFullYear()).slice(2); // Get last two digits of the year
+	const month = String(currentDate.getMonth() + 1).padStart(2, '0'); // Month is zero-based
+	const day = String(currentDate.getDate()).padStart(2, '0');
+	const datePrefix = `${year}${month}${day}_`;
+	fileName_base = datePrefix + fileName_base; // Prepend the date to the file name
 
 	const templateRelPath = $.getenv("template_note_path") || "";
 	const newNoteLocation = $.getenv("new_note_location") || "";
-	const newNoteRelPath = `${newNoteLocation}/${fileName}.md`;
+	const newNoteRelPath = `${newNoteLocation}/${fileName_base}.md`;
 	let newNoteAbsPath = `${vaultPath}/${newNoteRelPath}`;
 	while (fileExists(newNoteAbsPath)) {
 		newNoteAbsPath = newNoteAbsPath.slice(0, -3) + " 1.md";
 	}
 
-	let newNoteContent = "";
+	let newNoteContent = "# " + title + "\n\n";
 	if (templateRelPath) {
 		let templateAbsPath = `${vaultPath}/${templateRelPath}`;
 		if (!templateAbsPath.endsWith(".md")) templateAbsPath += ".md";
-		newNoteContent = readFile(templateAbsPath).replace("{{title}}", fileName); // insert title
+		newNoteContent = readFile(templateAbsPath).replace("{{title}}", fileName_base); // insert title
 	}
 
 	writeToFile(newNoteAbsPath, newNoteContent);
